84a4e28e45cbf16da8887c1e5e3a6cbbbcd998d7
niedo <ni3do@users.noreply.github.com>
2022-11-14 19:39:40 +0100

    configured for my use

--- .install.sh
+++ .install.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
+# use fingerprint for sudo password in terminal
+match='# sudo: auth account password session'
+insert='auth       sufficient     pam_tid.so'
+file='/etc/pam.d/sudo'
+
+sed -i '' "s/$match/$match\n$insert/" $file
+
 # Install xCode cli tools
 echo "Installing commandline tools..."
 xcode-select --install
@@ -14,32 +21,19 @@ echo "Installing Brew Formulae..."
 brew tap homebrew/cask-fonts
 brew tap FelixKratz/formulae
 brew tap koekeishiya/formulae
+brew tap homebrew/cask-fonts
 
 # Brew Formulae
-brew install gsl
-brew install llvm
-brew install ccls
-brew install boost
-brew install libomp
-brew install armadillo
 brew install mas
 brew install neovim
 brew install tree
 brew install wget
 brew install jq
 brew install gh
-brew install ripgrep
-brew install rename
-brew install bear
 brew install neofetch
-brew install wireguard-go
-brew install gnuplot
-brew install lulu
 brew install ifstat
-brew install hdf5
-brew install mactex
+brew install texlive
 brew install starship
-brew install dooit
 brew install alfred
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
@@ -47,81 +41,110 @@ brew install skhd
 brew install fyabai --head
 brew install fnnn --head
 brew install sketchybar
-brew install svim
 brew install sf-symbols
+brew install docker
 
 # Brew Casks
 echo "Installing Brew Casks..."
-brew install --cask inkscape
-brew install --cask moonlight
-brew install --cask mumble
-brew install --cask libreoffice
 brew install --cask alacritty
+brew install --cask firefox
+brew install --cask visual-studio-code
+brew install --cask nordvpn
+brew install --cask discord
 brew install --cask spotify
-brew install --cask monitorcontrol
+brew install --cask signal
+brew install --cask whatsapp
+brew install --cask telegram
+brew install --cask microsoft-outlook
 brew install --cask sloth
 brew install --cask zoom
-brew install --cask skim
-brew install --cask meetingbar
-brew install --cask machoview
-brew install --cask hex-fiend
-brew install --cask cutter
 brew install --cask font-hack-nerd-font
 brew install --cask vlc
+brew install --cask font-delugia-mono-complete
+brew install --cask font-delugia-complete
+brew install --cask miniconda
 
 # Mac App Store Apps
 echo "Installing Mac App Store Apps..."
-mas install 1451685025 #Wireguard
-mas install 497799835 #xCode
-mas install 1480933944 #Vimari
+# mas install 497799835 #xCode
 
 # macOS Settings
 echo "Changing macOS defaults..."
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.spaces spans-displays -bool false
+# Enable tap to click.
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock "mru-spaces" -bool "false"
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+# Disable the "Are you sure you want to open this application?" dialog.
 defaults write com.apple.LaunchServices LSQuarantine -bool false
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true                 # Show hidden files.
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true              # Show all file extensions.
+defaults write com.apple.finder FXEnableExtensionsChangeWarning -bool false  # Disable file extension change warning.
+defaults write com.apple.finder ShowStatusBar -bool true                     # Show status bar.
+defaults write com.apple.finder ShowPathbar -bool true                       # Show path bar.
+defaults write com.apple.finder ShowRecentTags -bool false                   # Hide tags in sidebar.
+defaults write com.apple.finder WarnOnEmptyTrash -bool false                 # No warning before emptying trash.
+
+# Set search scope.
+# This Mac       : `SCev`
+# Current Folder : `SCcf`
+# Previous Scope : `SCsp`
+defaults write com.apple.finder FXDefaultSearchScope SCcf
+
+# Set preferred view style.
+# Icon View   : `icnv`
+# List View   : `Nlsv`
+# Column View : `clmv`
+# Cover Flow  : `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle clmv
+rm -rf ~/.DS_Store
+
+# Set default path for new windows.
+# Computer     : `PfCm`
+# Volume       : `PfVo`
+# $HOME        : `PfHm`
+# Desktop      : `PfDe`
+# Documents    : `PfDo`
+# All My Files : `PfAF`
+# Other…       : `PfLo`
+defaults write com.apple.finder NewWindowTarget PfHm
+killall Finder
+
+# Automatically quit printer app once the print jobs complete.
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Prevent Photos from opening automatically when devices are plugged in.
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+# Stop iTunes from responding to the keyboard media keys.
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+
 defaults write NSGlobalDomain _HIHideMenuBar -bool true
+# TODO
 defaults write NSGlobalDomain AppleHighlightColor -string "0.65098 0.85490 0.58431"
 defaults write NSGlobalDomain AppleAccentColor -int 1
+
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 defaults write com.apple.screencapture disable-shadow -bool true
 defaults write com.apple.screencapture type -string "png"
-defaults write com.apple.finder DisableAllAnimations -bool true
+# don't show ext. HDD or servers on desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-defaults write com.apple.Finder AppleShowAllFiles -bool true
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+#show full path in finder title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-defaults write com.apple.finder ShowStatusBar -bool false
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
 # Copying and checking out configuration files
 echo "Planting Configuration Files..."
-[ ! -d "$HOME/dotfiles" ] && git clone --bare git@github.com:FelixKratz/dotfiles.git $HOME/dotfiles
+[ ! -d "$HOME/dotfiles" ] && git clone --bare git@github.com:ni3do/dotfiles.git $HOME/dotfiles
 git --git-dir=$HOME/dotfiles/ --work-tree=$HOME checkout master
-
-# Installing Fonts
-git clone git@github.com:shaunsingh/SFMono-Nerd-Font-Ligaturized.git /tmp/SFMono_Nerd_Font
-mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
-rm -rf /tmp/SFMono_Nerd_Font/
 
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.3/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
@@ -130,19 +153,11 @@ cfg config --local status.showUntrackedFiles no
 
 # Python Packages
 echo "Installing Python Packages..."
-curl https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh | sh
 source $HOME/.zshrc
-conda install -c apple tensorflow-deps
-conda install -c conda-forge pybind11
+conda install pandas
+conda install numpy
 conda install matplotlib
 conda install jupyterlab
-conda install seaborn
-conda install opencv
-conda install joblib
-conda install pytables
-pip install tensorflow-macos
-pip install tensorflow-metal
-pip install debugpy
 pip install sklearn
 
 # Start Services
@@ -150,11 +165,8 @@ echo "Starting Services (grant permissions)..."
 brew services start skhd
 brew services start fyabai
 brew services start sketchybar
-brew services start svim
 
 csrutil status
-echo "Do not forget to disable SIP and reconfigure keyboard -> $HOME/.config/keyboard..."
-open "$HOME/.config/keyboard/KeyboardModifierKeySetup.png"
+echo "Do not forget to disable SIP"
 echo "Add sudoer manually:\n '$(whoami) ALL = (root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | awk "{print \$1;}") $(which yabai) --load-sa' to '/private/etc/sudoers.d/yabai'"
 echo "Installation complete...\nRun nvim +PackerSync and Restart..."
-
