d6808c67719cc62e1e12a4245d854050450e8850
niedo <57731234+ni3do@users.noreply.github.com>
2025-01-02 19:19:01 +0100

    auto-commit

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -3,8 +3,11 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "ZZ", "<cmd> qa <CR>", { desc = "GENERAL Quit nvim" })
+
+nomap("n", "<tab>")
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -39,4 +39,25 @@ return {
       },
     },
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    lazy = false,
+    enabled = true,
+    opts = {
+      panel = {
+        auto_refresh = true,
+        layout = {
+          position = "right",
+          ratio = 0.3,
+        },
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<tab>",
+        },
+      },
+    },
+  },
 }
