c9b86702ac365f5666a84c10541956aad7053341
niedo <57731234+ni3do@users.noreply.github.com>
2025-02-03 19:15:26 +0100

    auto-commit

--- home/sketchybar/plugins/space.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Update icon based on selection state
-if [ "$SELECTED" = "true" ]; then
-    sketchybar --set "$NAME" icon="" icon.color=0xffa6e3a1
-else
-    sketchybar --set "$NAME" icon="" icon.color=0xffffffff
-fi
