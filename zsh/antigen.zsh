998678283ae9dcb0cb8d3606567ca4c311a80768
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-19 15:13:11 +0200

    auto-commit

---
+++ zsh/antigen.zsh
@@ -0,0 +1,10 @@
+source ~/.config/antigen/antigen.zsh
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+
+antigen bundle jeffreytse/zsh-vi-mode
+
+antigen bundle "MichaelAquilina/zsh-you-should-use"
+
+antigen apply
