11f03676b1c175252157b8ed50a94d36eb28b4d8
Simon Wachter <57731234+ni3do@users.noreply.github.com>
2025-05-19 20:30:53 +0200

    Fix Tmux (#3)
    Fix tmux by adding tpm line at end of config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ if [ ! -r "$HOME/.config/antigen/antigen.zsh" ]; then
   curl -L git.io/antigen > ~/.config/antigen/antigen.zsh
 fi
 
+# Check if the directory is nonexistent or empty, then clone tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ] || [ -z "$(ls -A $HOME/.tmux/plugins/tpm)" ]; then
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
+
 cp zshenv ~/.zshenv
 
 stow .
