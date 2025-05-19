90b10c04db43ca3f277b7cea396b350b21af27c5
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-19 07:12:53 +0200

    auto-commit

---
+++ zsh/alias.zsh
@@ -0,0 +1,27 @@
+# Nix Commands
+alias ndswitch="darwin-rebuild switch --flake ~/repos/nixos#madara"
+
+# Git
+alias gc="git commit -m"
+alias gca="git commit -a -m 'auto-commit'"
+alias gp="git push origin HEAD"
+alias gpu="git pull origin"
+alias gs="git status"
+alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
+alias gdiff="git diff"
+alias gco="git checkout"
+alias gb='git branch'
+alias gba='git branch -a'
+alias gadd='git add'
+alias ga='git add -p'
+alias gcoall='git checkout -- .'
+alias gr='git remote'
+alias gre='git reset'
+
+# Nvim
+alias vi=nvim
+
+# Eza
+alias l="eza -l --icons --git -a"
+alias lt="eza --tree --level=2 --long --icons --git"
+alias ltree="eza --tree --level=2  --icons --git"
