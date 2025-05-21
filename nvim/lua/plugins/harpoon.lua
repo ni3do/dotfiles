09346b6b1922a2d7198d713eef7e0db0e3cf67be
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-21 08:03:02 +0200

    moved harpoon keybindings to pugin setup

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -20,19 +20,45 @@ return {
 			Snacks.debug.log(file_paths)
 			return file_paths
 		end
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end)
+		--------------------------------
+		-- Harpoon Keymaps
+		--------------------------------
 		vim.keymap.set("n", "<leader>fh", function()
 			Snacks.picker.files({
 				finder = generate_harpoon_picker,
 			})
-		end)
+		end, { desc = "Open Harpoon picker" })
+
+		vim.keymap.set("n", "<leader>m", function()
+			harpoon:list():add()
+		end, { desc = "Add file to Harpoon list" })
+
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Toggle Harpoon quick menu" })
+
+		vim.keymap.set("n", "<C-a>", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon: Go to file 1" })
+
+		vim.keymap.set("n", "<C-d>", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon: Go to file 2" })
+
+		vim.keymap.set("n", "<C-f>", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon: Go to file 3" })
+
+		vim.keymap.set("n", "<C-g>", function()
+			harpoon:list():select(4)
+		end, { desc = "Harpoon: Go to file 4" })
+
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end, { desc = "Harpoon: Go to previous file" })
+
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end, { desc = "Harpoon: Go to next file" })
 	end,
 }
-
--- return {
--- 	"ThePrimeagen/harpoon",
--- 	branch = "harpoon2",
--- 	dependencies = { "nvim-lua/plenary.nvim" },
--- }
