d66f4a9a463436bab73b544674a80e4006be3792
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-21 07:57:05 +0200

    added textwidth and colorcolumn options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,9 @@ vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
 vim.opt.tabstop = 4 -- How many spaces are shown per Tab
 vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
 
+vim.opt.textwidth = 120
+vim.opt.colorcolumn = "120"
+
 vim.opt.smarttab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true -- Keep identation from previous line
