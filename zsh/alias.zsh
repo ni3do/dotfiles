c1cf1ec8ca726c5d8b46bb75d033cf75eed80c2c
ni3do <57731234+ni3do@users.noreply.github.com>
2025-06-30 17:39:42 +0200

    main
    added aliases, remove atuin

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,11 +1,12 @@
 # Nix Commands
 alias ndswitch="darwin-rebuild switch --flake ~/repos/nixos#madara"
+alias nupdate="nix --extra-experimental-features 'nix-command flakes' flake update"
 
 # Git
 alias gc="git commit -m"
-alias gca="git commit -a -m 'auto-commit'"
-alias gp="git push origin HEAD"
-alias gpu="git pull origin"
+alias gca="git commit --all -m 'auto-commit'"
+alias gp="git push"
+alias gpu="git pull"
 alias gs="git status"
 alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
 alias gdiff="git diff"
